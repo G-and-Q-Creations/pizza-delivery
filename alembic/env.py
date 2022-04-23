995b5582026874bf82acd8983df031c6c8fff6c8
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2022-04-23 12:18:05 -0700

    Use environment variables for setting db connection url

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,11 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from sqlalchemy.engine import create_engine
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -25,6 +27,14 @@ target_metadata = None
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+def get_url():
+    return "postgresql://%s:%s@%s/%s" % (
+        os.getenv("PIZZA_DELIVERY_DB_USER"),
+        os.getenv("PIZZA_DELIVERY_DB_PASSWORD"),
+        os.getenv("PIZZA_DELIVERY_DB_HOST"),
+        os.getenv("PIZZA_DELIVERY_DB_NAME")
+    )
+
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -38,7 +48,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,11 +67,7 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = create_engine(get_url())
 
     with connectable.connect() as connection:
         context.configure(
